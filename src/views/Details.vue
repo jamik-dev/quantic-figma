f87867969a92fe2a492e5a668e6fd291495cc0b9
jamik-dev <jamik.oik.group@gmail.com>
2023-01-17 19:13:31 +0500

    preloader added

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="mx-auto w-[80%]">
+    <transition v-if="isLoading" name="fade">
+      <Preloader />
+    </transition>
     <AboutSection />
     <MessageButton />
   </div>
@@ -8,14 +11,35 @@
 <script setup>
 import AboutSection from '../components/local/AboutSection.vue';
 import MessageButton from '../components/local/MessageButton.vue';
+import Preloader from '../components/global/Preloader.vue';
+import { onMounted, ref } from 'vue';
 
-import { onMounted } from 'vue';
+const isLoading = ref(true)
 
+function preloader() {
+  setTimeout(() => {
+    isLoading.value = false;
+  }, 300)
+}
 function scrollToTop() {
   window.scrollTo(0,0);
 }
-
 onMounted(() => {
+  preloader();
   scrollToTop();
 })
 </script>
+
+<style scoped>
+  .fade-enter-from, .fade-leave-to {
+    opacity: 0;
+  }
+
+  .fade-enter-to, .fade-leave-from {
+    opacity: 1;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: all 0.2s linear;
+  }
+</style>
