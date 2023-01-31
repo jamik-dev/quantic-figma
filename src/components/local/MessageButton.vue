990a83b6c6689a3127d558018d66f0e4a52cf89f
jamik-dev <jamik.oik.group@gmail.com>
2023-01-31 23:59:44 +0500

    format

--- src/components/local/MessageButton.vue
+++ src/components/local/MessageButton.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="fixed right-4 lg:right-16 bottom-8 z-10 animate-bounce cursor-pointer">
-    <div class="w-[72px] h-[72px] rounded-[100%] bg-gradient-to-r from-gradientfrom to-gradientto flex items-center justify-center"><img src="../../assets/img/chat.svg" alt=""></div>
+    <div
+      class="w-[72px] h-[72px] rounded-[100%] bg-gradient-to-r from-gradientfrom to-gradientto flex items-center justify-center">
+      <img src="../../assets/img/chat.svg" alt=""></div>
   </div>
 </template>
 
@@ -8,4 +10,5 @@
 </script>
 
 <style scoped>
+
 </style>
